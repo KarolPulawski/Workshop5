8d85cd36ff5ae2b11ab692def953831f7f9f7161
KarolPulawski <karol.j.pulawski@gmail.com>
2018-08-24 15:51:26 +0200

    First message

---
+++ src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -0,0 +1,10 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDiApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    }
+}
